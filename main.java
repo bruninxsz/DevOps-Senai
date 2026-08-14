2a707dc5928cc3ba94f80218a8888b538be73185
bruninxsz <brunohsp0306@gmail.com>
2026-08-13 22:38:30 -0300

    Aula 02 DevOps - Primeira Pipeline

--- main.java
+++ main.java
@@ -1,3 +1,8 @@
-public class main {
+public class Main {
     
+    public static void main (String[] args) {
+        System.out.println("Olá, Devops");
+        System.out.println("Pipeline executada com sucesso!");
+    }
+
 }
